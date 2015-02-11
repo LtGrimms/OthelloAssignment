aecda8221a0ebfbdd7e51a5fd6836797c095c707
Justin Chu <darkmephisto@gmail.com>
2015-02-11 16:32:43 -0700

    Deleted lines of code that inverted the board when the player changed, this is now done by the find all moves function

--- Othello.hs
+++ Othello.hs
@@ -107,8 +107,6 @@ pickFirst :: Chooser -- ^ Takes in a Chooser (which returns a Maybe (Int,Int))
 pickFirst (GameState {play = p, theBoard = b}) c
 	|length (findAllMovesAndCaptures b c) == 0 = []
 	|length (findAllMovesAndCaptures b c) /= 0 = mapJust (head (findAllMovesAndCaptures b c))
---	|length (findAllMovesAndCaptures (invertBoardPieces b)) == 0 && c == W = []
---	|length (findAllMovesAndCaptures (invertBoardPieces b)) /= 0 && c == W = mapJust (head (findAllMovesAndCaptures (invertBoardPieces b)))
 
 --	|Random strategy that chooses a random move contained from all valid moves
 randomStrategy :: Int -- ^ Takes in an Int representing the random number
@@ -116,8 +114,6 @@ randomStrategy :: Int -- ^ Takes in an Int representing the random number
 randomStrategy random (GameState {play = p, theBoard = b}) c
 	|length (findAllMovesAndCaptures b c) == 0 = []
 	|length (findAllMovesAndCaptures b c) /= 0 = mapJust (pickRandom (findAllMovesAndCaptures b c) random)
---	|length (findAllMovesAndCaptures (invertBoardPieces b)) == 0 && c == W = []
---	|length (findAllMovesAndCaptures (invertBoardPieces b)) /= 0 && c == W = mapJust (pickRandom (findAllMovesAndCaptures (invertBoardPieces b)) random)
 
 
 
